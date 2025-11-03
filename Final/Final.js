4b0ce62820b43f6726d04dad44b902174e3c226f
JOAO VICTOR CAMPOS MONTI <05970646016@senacrs.edu.br>
2025-11-03 09:48:05 -0300

    Teste de Envio de Arquivo

---
+++ Final/Final.js
@@ -0,0 +1,33 @@
+<script>
+       
+         
+    var imagens= [
+        "1.jpg ",
+        "2.jpg",
+        "3.png" ,
+    ]
+    var indice = 0;
+ 
+    var banner = document.getElementById('banner');
+    function mostrarimagens(){
+        banner.src = imagens[indice];
+    }
+    function ir(){
+        //incrementar o indice
+        indice++;
+        if(indice >= imagens.length){
+            indice = 0;
+        }
+        mostrarimagens();
+    }
+    function voltar(){
+        indice--;
+        if(indice < 0 ){
+        indice = imagens.length - 1;
+        }
+        mostrarimagens();
+    }
+   
+ 
+ 
+    </script>
